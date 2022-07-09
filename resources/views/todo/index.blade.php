68d54eff02992f72b4779770ae09c3c6b809d0ee
abol313 <FazlAbol18@gmail.com>
2022-07-09 18:32:05 +0430

    make view.todo.index

---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    @forelse($todos as $todo)
+        <x-todo.item :todo="$todo"/>
+    @empty
+        <h3>No todo!</h3>
+    @endforelse
+@endsection
