949977492ebd9448a1d2e5cf0664af0dff718591
abol313 <FazlAbol18@gmail.com>
2022-07-09 19:22:18 +0430

    copy form content from create view page

--- resources/views/todo/edit.blade.php
+++ resources/views/todo/edit.blade.php
@@ -2,4 +2,45 @@
 
 @section('content')
     <h2>Edit the todo !</h2>
+    @extends('layouts.app')
+
+@section('content')
+    <h2>Make a todo !</h2>
+
+    <form action="{!!route('todos.update')!!}" method="post">
+        @csrf
+
+        <label for="title">Title</label>
+        <input id="title" name="title" value="{{old('title')}}" minlength="5" maxlength="100" placeholder="title" title="pass a title for your todo" required/>
+        @error('title')
+            <div class="error-input">
+                <h3>{{$message}}</h3>
+            </div>
+        @enderror
+
+        <label for="description">Description</label>
+        <textarea id="description" name="description" rows="10" maxlength="5" placeholder="description" title="get description for more details...">{{@old('description')}}</textarea>
+        @error('description')
+            <div class="error-input">
+                <h3>{{$message}}</h3>
+            </div>
+        @enderror
+        
+        <label for="category">Category</label>
+        <select id="category" name="category" placeholder="category" title="select your category">
+            @foreach($categories as $category)
+                <option value="{{$category->id}}" @selected(session('_old_input.category') == $category->id)>{{$category->name}}</option>
+            @endforeach
+        </select>
+        @error('category')
+            <div class="error-input">
+                <h3>{{$message}}</h3>
+            </div>
+        @enderror
+    
+        <input type="submit" value="Make !"/>
+
+    </form>
+
+@endsection
 @endsection
