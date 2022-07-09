c99991fc5a2aef04283caeab4ced4321967a3f6f
abol313 <FazlAbol18@gmail.com>
2022-07-09 12:26:12 +0430

    add categories table migration

---
+++ database/migrations/2022_07_09_061809_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',14);
+            $table->timestamps();
+        });
+
+        if(Schema::hasTable('todos'))
+            Schema::table('todos',function(Blueprint $table){
+                $table->foreign('category')->references('id')->on('categories');
+            });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
