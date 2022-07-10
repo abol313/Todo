50fec76c50ad839f61f66f1900a9985a605f52e8
abol313 <FazlAbol18@gmail.com>
2022-07-10 18:03:20 +0430

    fix get all properties in Category::all()

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -16,7 +16,7 @@ class CategoryController extends Controller
     public function index()
     {
         //
-        return view('category.index',['categories'=>Category::all(['name'])]);
+        return view('category.index',['categories'=>Category::all()]);
     }
 
     /**
