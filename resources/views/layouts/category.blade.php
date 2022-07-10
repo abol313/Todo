3dc7c5379e124f5407beac843818f404c56b0d3a
abol313 <FazlAbol18@gmail.com>
2022-07-10 17:32:26 +0430

    add category layout

---
+++ resources/views/layouts/category.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('title','Category Collection')
+
+
+@section('header')
+    <h1>Welcome to Category Collection !</h1>
+@endsection
+
+@section('navbar')
+    <li>
+        <a href="{{route('todos.index')}}">
+            <h2>Todos</h2>
+        </a>
+    </li>
+
+    <li>
+        <a href="{{route('todos.index')}}">
+            <h2>List</h2>
+        </a>
+    </li>
+
+    <li>
+        <a href="{{route('todos.create')}}">
+            <h2>Make</h2>
+        </a>
+    </li>
+@endsection
+
+@section('sidebar')
+
+@endsection
+
+@section('content')
+
+@endsection
+
