19ef10d57cbf388b39eb6a2fa2f9fac26827ebfe
abol313 <FazlAbol18@gmail.com>
2022-07-10 18:57:39 +0430

    fix names of TodoRequest classes

--- app/Http/Requests/StoreTodoRequest.php
+++ app/Http/Requests/StoreTodoRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class TodoStoreRequest extends FormRequest
+class StoreTodoRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
