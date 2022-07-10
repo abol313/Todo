bdfb4c75baaa1625d210b5ee4a0839061f3b8ca7
abol313 <FazlAbol18@gmail.com>
2022-07-10 15:48:28 +0430

    fix the length limitation of title&textarea

--- resources/views/todo/edit.blade.php
+++ resources/views/todo/edit.blade.php
@@ -8,7 +8,7 @@
         @method('put')
 
         <label for="title">Title</label>
-        <input id="title" name="title" value="{{old('title')}}" minlength="0" maxlength="100" placeholder="title" title="pass a title for your todo" required/>
+        <input id="title" name="title" value="{{old('title') ?? $todo->title}}" minlength="5" maxlength="100" placeholder="title" title="pass a title for your todo" required/>
         @error('title')
             <div class="error-input">
                 <h3>{{$message}}</h3>
@@ -16,7 +16,7 @@
         @enderror
 
         <label for="description">Description</label>
-        <textarea id="description" name="description" rows="10" maxlength="5" placeholder="description" title="get description for more details...">{{@old('description')}}</textarea>
+        <textarea id="description" name="description" rows="10" maxlength="65535" placeholder="description" title="get description for more details...">{{@old('description') ?? $todo->description}}</textarea>
         @error('description')
             <div class="error-input">
                 <h3>{{$message}}</h3>
@@ -26,7 +26,7 @@
         <label for="category">Category</label>
         <select id="category" name="category" placeholder="category" title="select your category">
             @foreach($categories as $category)
-                <option value="{{$category->id}}" @selected(session('_old_input.category') == $category->id)>{{$category->name}}</option>
+                <option value="{{$category->id}}" @selected(session('_old_input.category') == $category->id || $todo->category == $category->id)>{{$category->name}}</option>
             @endforeach
         </select>
         @error('category')
