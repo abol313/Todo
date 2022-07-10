c07a71c4f6489edb1d50a1b88398cdcceb4c6da4
abol313 <FazlAbol18@gmail.com>
2022-07-10 17:36:10 +0430

    handle CategoryController@index

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -16,6 +16,7 @@ class CategoryController extends Controller
     public function index()
     {
         //
+        return view('category.index',['categories'=>Category::all(['name'])]);
     }
 
     /**
