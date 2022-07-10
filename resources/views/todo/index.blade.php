45ac6708f6e3684454d842bcbec2ba216adfbcd0
abol313 <FazlAbol18@gmail.com>
2022-07-10 22:42:26 +0430

    add filter part in sidebar

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -7,3 +7,18 @@
         <h3>No todo!</h3>
     @endforelse
 @endsection
+
+@section('sidebar')
+
+    <form action="{{route('todos.filter')}}">
+            
+        @foreach($categories as $category)
+            <label for="todo-filter-category-{{$category->name}}">{{$category->name}}</label>
+            <input id="todo-filter-category-{{$category->name}}" name="categories[]" value="{{$category->name}}" type="checkbox" title="filter todos with category {{$category->name}}" /> 
+        @endforeach
+
+        <input type="submit" value="filter"/>
+
+    </form>
+
+@endsection
