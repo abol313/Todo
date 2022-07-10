1055f11d5973abb0b7451a5719aa5c9a6b74ef12
abol313 <FazlAbol18@gmail.com>
2022-07-10 17:36:30 +0430

    fix the wanted property

--- resources/views/components/category/item.blade.php
+++ resources/views/components/category/item.blade.php
@@ -4,6 +4,6 @@
 
 <a href="{{route('categories.edit',['category'=>$todo])}}" target="_blank">
     <div class="category-item">
-        <strong>{{$category->title}}</strong>
+        <strong>{{$category->name}}</strong>
     </div>
 </a>
