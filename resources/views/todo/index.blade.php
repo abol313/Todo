2f3b0721288a7525fdde0f68a00542ca13e2b9da
abol313 <FazlAbol18@gmail.com>
2022-07-11 11:57:27 +0430

    add reset input and No category item (category:null)

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -11,13 +11,16 @@
 @section('sidebar')
 
     <form action="{{route('todos.filter')}}">
-            
+
+        <label for="todo-filter-no-category">No category</label>
+        <input id="todo-filter-no-category" class="todo-filter-category" name="filter[categories][]" value="" type="checkbox" title="filter todos with no category" @checked( in_array(null, session('todo.filter.categories') ?? [], true))/> 
+
         @foreach($categories as $category)
             <label for="todo-filter-category-{{$category->name}}">{{$category->name}}</label>
-            <input id="todo-filter-category-{{$category->name}}" name="filter[categories][]" value="{{$category->id}}" type="checkbox" title="filter todos with category {{$category->name}}" @checked(in_array($category->id,session('todo.filter.categories',[])))/> 
+            <input id="todo-filter-category-{{$category->name}}" class="todo-filter-category" name="filter[categories][]" value="{{$category->id}}" type="checkbox" title="filter todos with category {{$category->name}}" @checked(in_array($category->id.'',session('todo.filter.categories') ?? [], true))/> 
         @endforeach
 
-        <input type="reset" value="Clear"/>
+        <input type="reset" onclick="document.querySelectorAll('.todo-filter-category').forEach(v => v.removeAttribute('checked'))" value="Clear"/>
         <input type="submit" value="filter"/>
 
     </form>
