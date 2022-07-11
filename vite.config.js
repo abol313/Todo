9a4fdfbea68187501e4d13b9fb864fdeec5acd69
abol313 <FazlAbol18@gmail.com>
2022-07-11 18:36:46 +0430

    config vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel([
+            'resources/css/colors.css',
+            'resources/css/light.css',
+            'resources/css/dark.css',
             'resources/css/app.css',
             'resources/css/todo.css',
             'resources/css/category.css',
