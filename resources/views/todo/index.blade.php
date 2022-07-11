a27b7b209fd1ce2b69cb7e95e6c916614cbc1033
abol313 <FazlAbol18@gmail.com>
2022-07-11 20:54:11 +0430

    pass classes to style

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -9,20 +9,25 @@
 @endsection
 
 @section('sidebar')
+    <div class="filter">
+        <div class="categories">
+            <form action="{{route('todos.filter')}}">
 
-    <form action="{{route('todos.filter')}}">
+                <div class="category">
+                    <label for="todo-filter-no-category">No category</label>
+                    <input id="todo-filter-no-category" class="todo-filter-category" name="filter[categories][]" value="" type="checkbox" title="filter todos with no category" @checked( in_array(null, session('todo.filter.categories') ?? [], true))/> 
+                </div>
+                @foreach($categories as $category)
+                    <div class="category">
+                        <label for="todo-filter-category-{{$category->name}}">{{$category->name}}</label>
+                        <input id="todo-filter-category-{{$category->name}}" class="todo-filter-category" name="filter[categories][]" value="{{$category->id}}" type="checkbox" title="filter todos with category {{$category->name}}" @checked(in_array($category->id.'',session('todo.filter.categories') ?? [], true))/> 
+                    </div>
+                @endforeach
 
-        <label for="todo-filter-no-category">No category</label>
-        <input id="todo-filter-no-category" class="todo-filter-category" name="filter[categories][]" value="" type="checkbox" title="filter todos with no category" @checked( in_array(null, session('todo.filter.categories') ?? [], true))/> 
+                <input type="reset" onclick="document.querySelectorAll('.todo-filter-category').forEach(v => v.removeAttribute('checked'))" value="Clear"/>
+                <input type="submit" value="filter"/>
 
-        @foreach($categories as $category)
-            <label for="todo-filter-category-{{$category->name}}">{{$category->name}}</label>
-            <input id="todo-filter-category-{{$category->name}}" class="todo-filter-category" name="filter[categories][]" value="{{$category->id}}" type="checkbox" title="filter todos with category {{$category->name}}" @checked(in_array($category->id.'',session('todo.filter.categories') ?? [], true))/> 
-        @endforeach
-
-        <input type="reset" onclick="document.querySelectorAll('.todo-filter-category').forEach(v => v.removeAttribute('checked'))" value="Clear"/>
-        <input type="submit" value="filter"/>
-
-    </form>
-
+            </form>
+        </div>
+    </div>
 @endsection
