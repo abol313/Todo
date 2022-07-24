f52687af4f923f8298887feaac0ab6409ad7da11
abol313 <FazlAbol18@gmail.com>
2022-07-24 11:18:23 +0430

    handle auth.session middleware on categories

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::redirect('/','/todos');
 Route::get('/theme',[ThemeController::class,'setTheme'])->name('theme');
 Route::get('/todos/filter',[TodoController::class,'filterIndex'])->name('todos.filter');
 Route::resource('todos',TodoController::class);
-Route::resource('categories',CategoryController::class)->middleware(['auth','verified']);
+Route::resource('categories',CategoryController::class)->middleware(['auth','auth.session','verified']);
 
 //Authentications
 Route::name('auth.')->group(function()
