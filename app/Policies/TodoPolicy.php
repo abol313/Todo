113279564a3d880abbc2fc59fd2fd2a2251488e1
abol313 <FazlAbol18@gmail.com>
2022-08-02 10:51:20 +0430

    handle Todo authorizations by TodoPolicy

---
+++ app/Policies/TodoPolicy.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Todo;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+class TodoPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(?User $user)
+    {
+        //
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(?User $user, Todo $todo)
+    {
+        //
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(?User $user)
+    {
+       //
+       return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(?User $user, Todo $todo)
+    {
+        return $todo->hasUser(null)===null ?: !!$user?->hasTodo($todo);   
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(?User $user, Todo $todo)
+    {
+        //
+        if( $user === null ) return !$todo->hasUser(null);
+    
+        return ( $todo->hasUser(null)===null ?: !!$user?->hasTodo($todo) )  
+        && now()->getTimestamp() - $todo->getAttribute($todo->getCreatedAtColumn())->timestamp > 10;
+
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(?User $user, Todo $todo)
+    {
+        //
+        return true;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(?User $user, Todo $todo)
+    {
+        //
+        return true;
+    }
+}
